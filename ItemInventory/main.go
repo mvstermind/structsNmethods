45c9f5e95c993afeaaf31bb8665b5914c86b7c2f
mvstermind <zablockidawid1@gmail.com>
2024-04-17 11:03:20 +0200

    i'm slowly getting the hang of structs and methods but in this repo i'll focus mostly on writing it so i can get really comfortable with this

--- ItemInventory/main.go
+++ ItemInventory/main.go
@@ -4,40 +4,92 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
-type Inventory struct {
-	Name     string
-	Quantity int
-	Price    int
-	ID       int
+type Person struct {
+	Name string
+	Age  int
 }
 
-func (n *Inventory) AddItem() {
+func (p *Person) Client() (bool, string) {
+	fmt.Print("Enter your name: ")
 	b := bufio.NewReader(os.Stdin)
+	name, _ := b.ReadString('\n')
 
-	fmt.Print("Enter product name: ")
-	n.Name, _ = b.ReadString('\n')
-	n.Name = strings.TrimSpace(n.Name)
+	if name != "" {
+		fmt.Print("Your age: ")
+		fmt.Scanf("%d\n", &p.Age)
+		switch {
+		case p.Age >= 18:
+			return true, name
+		case p.Age < 18:
+			return false, ""
+		}
+	} else {
+		return false, ""
+	}
+	return true, name
+}
 
-	fmt.Print("Item quantity: ")
-	fmt.Scanf("%d\n", &n.Quantity)
+type Shop struct {
+	Product  string
+	Quantity int
+	Adress   string
+	Price    int
+}
 
-	fmt.Print("Price: ")
-	fmt.Scanf("%d\n", &n.Price)
+func (s *Shop) delivery(name string) (string, int, string) {
+	if name != "" {
+		b := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Item ID: ")
-	fmt.Scanf("%d\n", &n.ID)
+		fmt.Print("Type product name: ")
+		s.Product, _ = b.ReadString('\n')
+		if s.Product == "\n" {
+			fmt.Println("You gotta pick a product")
+			os.Exit(2)
+		}
+
+		fmt.Printf("How many items you wanna get: ")
+		fmt.Scanf("%d\n", &s.Quantity)
+		if s.Quantity <= 0 {
+			fmt.Println("you gotta get somrthing")
+			os.Exit(3)
+		}
+
+		fmt.Print("Delivery adress: ")
+		s.Adress, _ = b.ReadString('\n')
+		if s.Adress == "\n" {
+			fmt.Println("Adress cannot be blank!")
+			os.Exit(3)
+		}
+
+		return s.Product, s.Quantity, s.Adress
+	} else {
+
+		fmt.Println("Invalid Name")
+		os.Exit(1)
+		return "", 0, ""
+	}
+}
+
+func (s *Shop) price(q int) int {
+	p := Shop{Price: 32}
+	total := p.Price * q
+	return total
 }
 
 func main() {
-	item := Inventory{}
-	item.AddItem()
-
-	fmt.Printf("Product name: %v\n", item.Name)
-	fmt.Printf("Item Quantity: %d\n", item.Quantity)
-	fmt.Printf("Item Price: %d\n", item.Price)
-	fmt.Printf("Item ID: %d\n", item.ID)
-
+	customer := Person{}
+	boo, name := customer.Client()
+	if boo == true {
+		shop := Shop{}
+		product, quantity, adress := shop.delivery(name)
+		fmt.Printf("Thanks for your order\n")
+		fmt.Printf("Product: %v\n", product)
+		fmt.Printf("Quantity: %v\n", quantity)
+		fmt.Printf("Your adress: %v\n", adress)
+		fmt.Printf("Total Price: %v\n", shop.price(quantity))
+	} else {
+		fmt.Println("sorry you can't order here")
+	}
 }
